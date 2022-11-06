4911816580957cb7905b31790249bc5ea510effe
Preet-Sojitra <preet.dev373@gmail.com>
2022-11-06 09:14:40 +0530

    dynamic cards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict"
-const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImthbGFzaGphaW4xMjRAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxMTM2NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMi0xMS0wMiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjY3NzAzNzAzLCJuYmYiOjE2Njc2OTY1MDN9.Igc_XuP8wTG0O79b9HccR0KdimrJbbxH5EByVDbLHVA';
+const TOKEN =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImthbGFzaGphaW4xMjRAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxMTM2NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMi0xMS0wMiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjY3NzAzNzAzLCJuYmYiOjE2Njc2OTY1MDN9.Igc_XuP8wTG0O79b9HccR0KdimrJbbxH5EByVDbLHVA"
 let symptoms
 let dictionary = []
 let options = []
@@ -80,29 +81,27 @@ function get() {
       console.log(chances)
       console.log(specialist)
 
-    //   const disease = document.getElementById("disease")
-    //   const accuracy = document.getElementById("accuracy")
-    //   const doctor = document.getElementById("doctor")
+      //   const disease = document.getElementById("disease")
+      //   const accuracy = document.getElementById("accuracy")
+      //   const doctor = document.getElementById("doctor")
 
       const cardsContainer = document.getElementById("cards")
-      const cards = []
+      let cards = []
 
       for (let i = 0; i < diseases.length; i++) {
         const card = document.createElement("div")
         card.classList.add("card")
-        card.innerHTML = `
-                <div class="card-body">
-                    <h1>You have ${chances[i]}% chance of having${diseases[i]} </h1> 
+        const template = `
+                <div class="card">
+                    <h1>You have ${chances[i]}% chance of having ${diseases[i]} </h1> 
                     <p>Specialist: You should consult the doctor having speciality in ${specialist[i]}</p>
+                    <button class="btn" onclick="">Find a doctor</button>
+                </div>
                     `
-        cards.push(card)
+        cards.push(template)
       }
-      console.log(cards)
-
-      //   cardsContainer.innerHTML = cards
-
-      //   console.log(disease.textContent)
-
+      const allCards = cards.join("")
+      cardsContainer.innerHTML = allCards
     })
   })
 }
