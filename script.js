cfae758d722d2286c10077734848ac77f027c98e
kalashjain23 <kalashjain513@gmail.com>
2022-11-06 06:40:12 +0530

    changing tokens and fixing bugs

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict"
-const TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImthbGFzaGphaW4xMjRAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxMTM2NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMi0xMS0wMiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjY3Njk1NjE3LCJuYmYiOjE2Njc2ODg0MTd9.egh43vfM4lrUY_UPnTKkbmUU8zzUnS0YLEzBTd5N-QU"
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImthbGFzaGphaW4xMjRAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxMTM2NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMi0xMS0wMiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjY3NzAzNzAzLCJuYmYiOjE2Njc2OTY1MDN9.Igc_XuP8wTG0O79b9HccR0KdimrJbbxH5EByVDbLHVA';
 let symptoms
 let dictionary = []
 let options = []
@@ -81,9 +80,9 @@ function get() {
       console.log(chances)
       console.log(specialist)
 
-      const disease = document.getElementById("disease")
-      const accuracy = document.getElementById("accuracy")
-      const doctor = document.getElementById("doctor")
+    //   const disease = document.getElementById("disease")
+    //   const accuracy = document.getElementById("accuracy")
+    //   const doctor = document.getElementById("doctor")
 
       const cardsContainer = document.getElementById("cards")
       const cards = []
@@ -93,7 +92,7 @@ function get() {
         card.classList.add("card")
         card.innerHTML = `
                 <div class="card-body">
-                    <h1>You have ${chances[i]}% chance of having${disease[i]} </h1> 
+                    <h1>You have ${chances[i]}% chance of having${diseases[i]} </h1> 
                     <p>Specialist: You should consult the doctor having speciality in ${specialist[i]}</p>
                     `
         cards.push(card)
@@ -104,9 +103,6 @@ function get() {
 
       //   console.log(disease.textContent)
 
-      // for(let i = 0; i < specialist.length; i++){
-      //     document.querySelector('#find-me').addEventListener('click', geoFindMe(specialist[i]));
-      // }
     })
   })
 }
